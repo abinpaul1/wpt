ad6336a403213c33291bc3407201149b35902aa3
Abin K Paul <abin.paul1@gmail.com>
2022-07-01 17:40:02 +0530

    WPT

---
+++ resource-timing/resources/importer_async.js
@@ -0,0 +1,4 @@
+import './fake_responses.py?url=empty_script.js?script-head-import-async';
+import('./fake_responses.py?url=empty_script.js?script-head-import-async-dynamic').then(module => {
+  console.log("Loaded a module");
+});
